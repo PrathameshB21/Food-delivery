2b4bedaca47b3fc1541a1cf99fde2e3fbd5613df
prathamesh borate <prathmeshborate21@gmail.com>
2024-10-04 18:19:51 +0530

    updated database config

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,13 +1,11 @@
 const mongoose = require('mongoose');
 require('dotenv').config()
-const MongoUrl=process.env.MongoDBUrl;
+const MongoUrl = process.env.MongoDBUrl;
 
-try{mongoose.connect(MongoUrl,{
-    useNewUrlParser: true,
-    useUnifiedTopology: true
-})}catch(error){
+try { mongoose.connect(MongoUrl) }
+catch (error) {
     console.log(error);
-    
+
 }
 const db = mongoose.connection;
 
@@ -16,7 +14,7 @@ db.on('connected', () => {
 })
 db.on('error', () => {
     console.log('Failed to establish connection')
-    
+
 });
 db.on('disconnected', () => {
     console.log('Connection stoped')
